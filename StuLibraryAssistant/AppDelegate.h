6d78563e1a2df68b6ef8dde2b6b9fc4ba9f3cfb5
yuchufang1 <yuchufang1@huya.com>
2019-03-12 22:35:34 +0800

    【初始化】初始化工程

---
+++ StuLibraryAssistant/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StuLibraryAssistant
+//
+//  Created by yu on 2019/3/1.
+//  Copyright © 2019 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StuLibraryAssistant/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StuLibraryAssistant
+//
+//  Created by yu on 2019/3/1.
+//  Copyright © 2019 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
